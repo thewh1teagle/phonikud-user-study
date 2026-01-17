b5bf8984757ef797b375200b0a7b55095b60d0d7
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2026-01-17 22:31:00 +0200

    link

--- web/src/routes/Home.tsx
+++ web/src/routes/Home.tsx
@@ -1,11 +1,17 @@
 import { Button } from '@/components/ui/button'
+import { Link } from 'react-router'
 
 function App() {
   
 
   return (
     <>
-      <Button>Click me</Button>
+      <div className="p-4">
+        <h1 className="text-2xl font-bold mb-4">Home</h1>
+        <Link to="/experimental/test-firebase">
+          <Button>Go to Experimental Page</Button>
+        </Link>
+      </div>
     </>
   )
 }
