c7d1ad7bf679adad6fe0acad998e72da4cfa4fb1
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2026-01-17 22:28:37 +0200

    Update Vite configuration for base path and adjust import case sensitivity in main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './routes/home.tsx'
+import App from './routes/Home.tsx'
 import TestFirebase from './routes/experimental/TestFirebase.tsx'
 import { BrowserRouter, Route, Routes } from "react-router";
 
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/phonikud-user-study/'>
   <Routes>
     <Route path="/" element={<App />} />
     <Route path="/experimental/test-firebase" element={<TestFirebase />} />
